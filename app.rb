4c32226aa220822f9f4be4c0ec54ce1235a31f01
Kriti Rai <kriti.rai@gmail.com>
2018-05-16 05:23:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 class App < Sinatra::Base
 
-	# get '/' do
-	# 	erb :index
-	# end
+	get '/' do
+		erb :index
+	end
 
 	get '/hello'do
 		erb :hello
+	end
+
+	get '/goodbye' do
+		<%=@name = "Joe"%>
+		erb :goodbye
 	end
 
 	get '/date' do
